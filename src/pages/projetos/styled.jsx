dcf588c87088f3130c91d04ff18845d56f6bbe23
vinicius-bispo1 <vini.bispo125@gmail.com>
2024-04-24 19:38:40 -0300

    adicionando o carrosel

--- src/pages/projetos/styled.jsx
+++ src/pages/projetos/styled.jsx
@@ -9,3 +9,21 @@ display: flex;
     flex-direction: column;
 }
 `
+export const Container = styled.section`
+border: solid 2px red;
+width: 100%;
+display: flex;
+justify-content: center;
+align-items: center;
+`
+
+export const BoxImg = styled.div`
+/* border: solid 2px blue; */
+width: 70vw;
+.rec .rec-arrow-right{
+    background-color: red;
+}
+img{
+    width: 20vw;
+}
+`
